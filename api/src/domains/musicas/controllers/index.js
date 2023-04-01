c9ab2d69922aedfbc8160f1856ddcfe69ce27aa2
Vinícius de Sousa Viana <viniciusvsv193@gmail.com>
2023-04-01 20:22:55 -0300

    Criamos duas requisições get.

--- api/src/domains/musicas/controllers/index.js
+++ api/src/domains/musicas/controllers/index.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const Musica = require("../models/Musica");
+const listaDeMusicas = require("../models/Musica");
 
-router.get("/", (req, res) => {
-    res.status(200).send(Musica);
+router.get("/Musicas", (req, res) => {
+    res.status(200).send(listaDeMusicas);
+});
+
+router.get("/Musicas/Pesquisa", (req, res) => {
+    const nome = req.query.nome;
+    const filtro = Musica.find(Musica => Musica.nome === nome);
+
+    if(!filtro)
+        res.status(404).json("Nenhuma música encontrada");
+    else
+        res.status(200).json(filtro);
 });
 
 module.exports = router;
