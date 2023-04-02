0a0fa30a492b461a6a3fbde994832ae87bbd97de
Gabriella <gabrielladelimadev@gmail.com>
2023-04-01 21:13:45 -0300

    delete: apagando const repetida

--- api/src/domains/musicas/controllers/index.js
+++ api/src/domains/musicas/controllers/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const Musica = require("../models/Musica");
-const listaDeMusicas = require("../models/Musica");
 
 router.get("/Musicas", (req, res) => {
     res.status(200).send(listaDeMusicas);
