9cbe60dee060f84183a9f412c93e896be96eaf6d
Gabriella <gabrielladelimadev@gmail.com>
2023-04-02 15:20:50 -0300

    fix: arrumando metodo put

--- api/src/domains/musicas/services/MusicaServices.js
+++ api/src/domains/musicas/services/MusicaServices.js
@@ -1,8 +1,8 @@
-const listaDeMusicas = require("../models/Musica");
+const Musica = require("../models/Musica");
 
 class MusicaServices {
     Pesquisar(nome) {
-        const filtro = listaDeMusicas.find(Musica => Musica.nome === nome);
+        const filtro = Musica.find(Musica => Musica.nome === nome);
 
         if(!filtro)
                 return false;
@@ -18,30 +18,29 @@ class MusicaServices {
             quantidadeDownloads: body.quantidadeDownloads
         }
     
-        listaDeMusicas.push(musica);
+        Musica.push(musica);
 
         return musica;
     }
 
     Atualizar(body){
         const musica = this.Pesquisar(body.nome);
-        const index = listaDeMusicas.indexOf(musica);
-
+    
         if(musica == false)
                 return false;
             else
-                listaDeMusicas.at(index).quantidadeDownloads = body.quantidadeDownloads;
+                musica.quantidadeDownloads = body.quantidadeDownloads;
                 return musica;
     }
 
     Remover(body){
         const musica = this.Pesquisar(body.nome);
-        const index = listaDeMusicas.indexOf(musica);
+        const index = Musica.indexOf(musica);
 
         if(musica == false)
                 return false;
             else
-                listaDeMusicas.splice(index, 1);
+                Musica.splice(index, 1);
                 return musica;
                 
     }
