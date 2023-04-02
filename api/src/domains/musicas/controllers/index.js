474e65d699525dfeea5139942cc8c143453b1fe0
Vinícius de Sousa Viana <viniciusvsv193@gmail.com>
2023-04-02 08:14:42 -0300

    Consertei um erro e troquei o método find

--- api/src/domains/musicas/controllers/index.js
+++ api/src/domains/musicas/controllers/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Musica = require("../models/Musica");
+const listaDeMusicas = require("../models/Musica");
 
 router.get("/Musicas", (req, res) => {
     res.status(200).send(listaDeMusicas);
@@ -8,7 +9,7 @@ router.get("/Musicas", (req, res) => {
 
 router.get("/Musicas/Pesquisa", (req, res) => {
     const nome = req.query.nome;
-    const filtro = Musica.find(Musica => Musica.nome === nome);
+    const filtro = Musica.filter(Musica => Musica.nome === nome);
 
     if(!filtro)
         res.status(404).json("Nenhuma música encontrada");
