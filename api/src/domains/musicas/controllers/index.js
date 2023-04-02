bac97ce74465efd2a7fafef6642e95025289ff48
Vinícius de Sousa Viana <viniciusvsv193@gmail.com>
2023-04-02 08:39:07 -0300

    Criei método post. Adiciona

--- api/src/domains/musicas/controllers/index.js
+++ api/src/domains/musicas/controllers/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const Musica = require("../models/Musica");
+const listaDeMusicas = require("../models/Musica");
 
-
+//Retorna todas as músicas da lista
 router.get("/Musicas", (req, res) => {
     res.status(200).send(Musica);
 });
-
+//Filtra a lista de músicas por um nome passado
 router.get("/Musicas/Pesquisa", (req, res) => {
     const nome = req.query.nome;
     const filtro = Musica.filter(Musica => Musica.nome === nome);
@@ -15,6 +16,17 @@ router.get("/Musicas/Pesquisa", (req, res) => {
         res.status(404).json("Nenhuma música encontrada");
     else
         res.status(200).json(filtro);
+});
+//Adiciona uma música à lista
+router.post("/Musicas/:nome/:artista/:genero/:downloads", (req, res) => {
+    const musica = {
+        nome: req.params.nome,
+        artista: req.params.artista,
+        genero: req.params.genero,
+        quantidadeDownloads: req.params.downloads
+    }
+
+    listaDeMusicas.push(musica);
 });
 
 module.exports = router;
