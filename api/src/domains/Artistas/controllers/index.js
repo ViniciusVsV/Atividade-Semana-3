754eed37957e13c62c8b6696597c457fcb4841c8
Vinícius de Sousa Viana <viniciusvsv193@gmail.com>
2023-04-07 14:59:37 -0300

    Add: SQL e tabela Artista

---
+++ api/src/domains/Artistas/controllers/index.js
@@ -0,0 +1,4 @@
+const router = require("express").Router();
+const Artista = require("../models/Artista");
+
+module.exports = router;
--- api/src/domains/Artistas/models/Artista.js
+++ api/src/domains/Artistas/models/Artista.js
@@ -1,2 +1,31 @@
-const sequelize = require('../../../../database/index');
-const {DataTypes} = require('sequelize');
+const sequelize = require("../../../../database/index");
+const {DataTypes} = require("sequelize");
+
+const Artista = sequelize.define("Artista", {
+    ID:{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    Nome:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    Nacionalidade:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    Foto:{
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
+
+Artista.sync({alter: true, force: false})
+    .then(() => {
+        console.log("Tabela de artistas foi (re)criadad");
+    })
+    .catch((erro) => console.log(erro));
+    
+module.exports = Artista;
