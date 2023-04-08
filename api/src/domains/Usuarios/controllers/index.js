2cd0297391c2c84a5faf164595860678f1f787d8
Gabriella <gabrielladelimadev@gmail.com>
2023-04-07 21:06:42 -0300

    feature: adicionando UsuarioServices

--- api/src/domains/Usuarios/controllers/index.js
+++ api/src/domains/Usuarios/controllers/index.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const Usuario = require('../models/Usuario');
+const UsuarioService = require('../services/UsuarioServices');
 
 router.post('/', async(req, res) =>{
     const body = req.body;
     try{
-        await Usuario.create(body);
-        return res.status(201).end();
+        await UsuarioService.criacao(body);
+        return res.status(201).json('Usuario criado com sucesso!');
     }catch{
         return res.status(400);
     }
