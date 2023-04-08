4193590ae7c2e327a9a6f46784cfe02456aad939
Vinícius de Sousa Viana <viniciusvsv193@gmail.com>
2023-04-08 11:56:49 -0300

    Feature: Delete para músicas

--- api/src/domains/musicas/controllers/index.js
+++ api/src/domains/musicas/controllers/index.js
@@ -4,7 +4,7 @@ const Musica = require("../models/Musica");
 const MusicaServices = require("../services/MusicaServices");
 
 //Adiciona uma música ao banco de dados
-router.post('/', async(req, res) =>{
+router.post('/create', async(req, res) =>{
     const body = req.body;
     try{
         await MusicaServices.criacao(body);
@@ -14,5 +14,15 @@ router.post('/', async(req, res) =>{
     }
 });
 
+//Remove uma música do banco de dados pelo id
+router.delete("/delete", async(req, res) => {
+    const id = req.body.id;
+    try{
+        await MusicaServices.remover(id);
+        res.status(200).json("Música removida");
+    }catch(error) {
+        res.status(404).send(error);
+    }
+})
 
 module.exports = router;
--- api/src/domains/musicas/services/MusicaServices.js
+++ api/src/domains/musicas/services/MusicaServices.js
@@ -4,6 +4,15 @@ class MusicaServices{
     async criacao(body){
         await Musica.create(body);
     }
+
+    async remover(id){
+        const musica = await Musica.findByPk(id);
+        if(!musica)
+            throw new Error("Música não encontrada");
+        else{
+            musica.destroy();
+        }
+    }
 };
 
 module.exports = new MusicaServices();
