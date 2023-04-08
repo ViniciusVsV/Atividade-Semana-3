2d1332af8162f0650e59e56065215539dd700d0a
Vinícius de Sousa Viana <viniciusvsv193@gmail.com>
2023-04-08 13:21:20 -0300

    feature: update Usuarios

--- api/src/domains/Usuarios/controllers/index.js
+++ api/src/domains/Usuarios/controllers/index.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const Usuario = require('../models/Usuario');
 const UsuarioService = require('../services/UsuarioServices');
 
-router.post('/', async(req, res) =>{
+//Adiciona um usuário ao banco de dados
+router.post('/create', async(req, res) =>{
     const body = req.body;
     try{
         await UsuarioService.criacao(body);
@@ -10,6 +11,17 @@ router.post('/', async(req, res) =>{
     }catch{
         return res.status(400);
     }
-})
+});
+
+//Atualiza as informações de um usuário no banco de dados
+router.put("/update", async(req, res) => {
+    const body = req.body;
+    try {
+        await UsuarioService.atualizar(body);
+        res.status(200).json("Usuário atualizado");
+    } catch(error) {
+        res.status(404).send(error);
+    }
+});
 
 module.exports = router;
--- api/src/domains/Usuarios/services/UsuarioServices.js
+++ api/src/domains/Usuarios/services/UsuarioServices.js
@@ -4,6 +4,25 @@ class UsuarioService{
     async criacao(body){
         await Usuario.create(body);
     }
+
+    async atualizar(body){
+        const usuario = Usuario.findByPk(body.id);
+        if(!usuario)
+            throw new Error("Usuário não encontrada");
+        else{
+            usuario = await Usuario.update(
+                {
+                    nome: body.nome,
+                    email: body.email,
+                    senha: body.senha,
+                    cargo: body.cargo
+                },
+                {
+                    where: {id: body.id}
+                }
+            );
+        }
+    }
 };
 
 module.exports = new UsuarioService();
