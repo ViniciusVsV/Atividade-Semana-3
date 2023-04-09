3658de052be697c66243b81d7ad0138e634fbd7c
Vinícius de Sousa Viana <viniciusvsv193@gmail.com>
2023-04-09 11:33:58 -0300

    feature: rota para listar musicas de um artista

--- api/src/domains/Artistas/controllers/index.js
+++ api/src/domains/Artistas/controllers/index.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const Artista = require('../models/Artista');
 const ArtistaService = require('../services/ArtistaServices');
 
+
+//Adiciona um artista ao banco de dados
 router.post('/criar', async(req, res) =>{
     const body = req.body;
     try{
@@ -9,6 +11,17 @@ router.post('/criar', async(req, res) =>{
         return res.status(201).json('Artista criado com sucesso!');
     }catch{
         return res.status(400);
+    }
+});
+
+//Lista todas as músicas de um artista pelo nome do artista
+router.get("/listarArtista", async(req, res) => {
+    const body = req.body;
+    try {
+        const musicas = await ArtistaService.listarArtista(body);
+        res.status(200).send(musicas);
+    } catch(erro){
+        res.status(404).send(erro);
     }
 })
 
--- api/src/domains/Artistas/services/ArtistaServices.js
+++ api/src/domains/Artistas/services/ArtistaServices.js
@@ -1,8 +1,18 @@
-const Artista = require('../models/Artista');
+const Artista = require("../../Artistas/models/Artista");
 
 class ArtistaService{
     async criar(body){
         await Artista.create(body);
+    }
+
+    async listarArtista(body){
+        const artista = Artista.findOne({where: {nome : body.nome}});
+        if(!artista)
+            throw new Error("Artista não existe");
+        else{
+            const musicas = artista.findAll({where: {idArtista: artista.id}});
+            return musicas;
+        }
     }
 };
 
