2634a28801ac17e56235915fe9ff76494b2502bc
Vinícius de Sousa Viana <viniciusvsv193@gmail.com>
2023-04-09 10:43:25 -0300

    fix: padronização

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 const app = require("./config/ExpressConfig");
 require('dotenv').config();
 
-const port = process.env.PORT;
-app.listen(port, console.log(`Servidor rodando na porta ${port}`));
+const porta = process.env.PORT;
+app.listen(porta, console.log(`Servidor rodando na porta ${porta}`));
