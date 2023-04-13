8a71e6bfd0d84eea1448012562d2ae3d725a13f6
Gabriella <gabrielladelimadev@gmail.com>
2023-04-13 10:35:48 -0300

    feature: adicionando next em musicaUsuario

--- api/src/domains/musicaUsuario/controllers/index.js
+++ api/src/domains/musicaUsuario/controllers/index.js
@@ -2,24 +2,24 @@ const router = require('express').Router();
 const musicaUsuarioService = require('../services/musicaUsuarioServices');
 
 //Adiciona um usuário ao banco de dados
-router.post('/criar', async(req, res) =>{
+router.post('/criar', async(req, res, next) =>{
     const body = req.body;
     try{
         await musicaUsuarioService.criar(body);
         return res.status(201).json('Agora você escuta musicas!!!');
     }catch(erro){
-        return res.status(400).send(erro);
+        next(erro);
     }
 });
 
 //Lista todas as músicas que um usuário escuta
-router.get("/listarUsuario", async(req, res) => {
+router.get("/listarUsuario", async(req, res, next) => {
     const _UsuarioId = req.body.UsuarioId;
     const musicas = await musicaUsuarioService.listarUsuario(_UsuarioId);
     try {
         res.status(200).json(musicas);
     }catch(erro){
-        res.status(404).send(erro);
+        next(erro);
     }
 });
 
