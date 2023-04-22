9613c37e5c45dc9839b6c4801e5a16e51b454134
Gabriella <gabrielladelimadev@gmail.com>
2023-04-21 21:37:10 -0300

    feature: criptografia e restricoes nas rotas

--- api/src/domains/Usuarios/services/UsuarioServices.js
+++ api/src/domains/Usuarios/services/UsuarioServices.js
@@ -1,35 +1,61 @@
 const Usuario = require('../models/Usuario');
 const QueryError = require("../../../../errors/QueryError");
+const PermissionError = require('../../../../errors/PermissionError');
+const bcrypt = require('bcrypt');
+const cargoUsuario = require('../../../../constants/cargoUsuario');
 
 class UsuarioService{
-    /** @brief Cria um usuário em que o cargo só pode ser admin ou user.*/
+
+    /** @brief Criptografa a senha. */
+    async criptografarSenha(senha){
+        const saltRounds = 10;
+        const senhaCriptografada = await bcrypt.hash(senha, saltRounds);
+
+        return senhaCriptografada;
+    }
+
+    /** @brief Cria um usuário em que o cargo só pode ser user.*/
     async criar(body){
+        if(body.cargo == cargoUsuario.ADMIN){
+            throw new PermissionError('Não é possível criar um usuário com o cargo de administrador');
+        };
+
         const usuario = await Usuario.findOne({where: {email: body.email}});
-        if(usuario)
+
+        if(usuario){
             throw new QueryError("E-mail já cadastrado");
-        if (body.cargo != 'admin' && body.cargo != 'user') 
+        };
+            
+        if (body.cargo != 'user'){
             throw new QueryError("Cargo inválido");
-        
-
-        await Usuario.create(body);
-    } 
-    /** @brief Atualiza um usuário já existente.*/
-    async atualizar(body){
-        let usuario = Usuario.findByPk(body.id);
-        if(!usuario)
-            throw new QueryError("Usuário não encontrado");
-
-        usuario = await Usuario.update(
-            {
+        } else{
+            const usuario = {
                 nome: body.nome,
                 email: body.email,
                 senha: body.senha,
-                cargo: body.cargo
-            },
-            {
-                where: {id: body.id}
-            }
-        );
+                cargo: body.cargo,
+            };
+    
+            usuario.senha = await this.criptografarSenha(body.senha);
+    
+            await Usuario.create(usuario);
+        }
+    }
+    /** @brief Atualiza um usuário já existente.*/
+    async atualizar(id, body, usuarioLogado){
+        const usuario = await this.getById(id);
+
+        if(usuarioLogado.cargo != cargoUsuario.ADMIN && usuarioLogado.id != id){
+            throw new PermissionError("Você não tem permisão para editar outro usuário!");
+        } 
+        if(body.cargo && usuarioLogado != cargoUsuario.ADMIN){
+            throw new PermissionError("Você não tem permissão para editar seu cargo!");
+        }
+        if(body.senha){
+            body.senha = await this.senhaCriptografada(body.senha);
+        }
+
+        await usuario.update(body);
     }
 
     /** @brief Remove um usuário.*/
